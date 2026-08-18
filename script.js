f883e9912e10b84108bd763a1a5e32718473172a
Stephen Idoko <idokostephen93@gmail.com>
2026-08-18 02:10:01 +0100

    continued work

--- script.js
+++ script.js
@@ -7,4 +7,28 @@ function showSidebar(){
       sidebar.style.display = 'none'
     }
 
- 
+const menuBtn = document.getElementById("menuBtn");
+    const navbar = document.querySelector(".navbar");
+
+    menuBtn.addEventListener("click", () => {
+
+      navbar.classList.toggle("active");
+
+    });
+
+
+    const subscribeForm =
+      document.getElementById("subscribeForm");
+
+    subscribeForm.addEventListener("submit", (e) => {
+
+      e.preventDefault();
+
+      const email =
+        document.getElementById("email").value;
+
+      console.log("Subscriber:", email);
+
+      alert("Thank you for subscribing!");
+
+      subscribeForm.reset();
